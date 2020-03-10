4d984bb66a27ad299d7e8f243a4b1ca20179920b
Andre Gloukhmantchouk <andregl@fb.com>
2020-03-10 12:39:47 -0700

    No export

--- src/memory_win.cpp
+++ src/memory_win.cpp
@@ -1,16 +1,6 @@
 #include <nan.h>
 #include <node.h>
 
-NAN_METHOD(open) {}
-
-NAN_METHOD(resize) {}
-
-NAN_METHOD(unlink) {}
-
-NAN_MODULE_INIT(Init) {
-  NAN_EXPORT(target, open);
-  NAN_EXPORT(target, resize);
-  NAN_EXPORT(target, unlink);
-}
+NAN_MODULE_INIT(Init) {}
 
 NODE_MODULE(shmmap, Init)
